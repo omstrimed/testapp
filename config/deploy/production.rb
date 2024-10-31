e8287a5d43aa02b334fb6ae508a7d9116fdee579
athinellis <athinellis@gmail.com>
2024-10-31 11:56:05 +0200

    _3

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,12 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+role :app, %w{deploy@164.90.165.189}
+role :web, %w{deploy@164.90.165.189}
+role :db,  %w{deploy@164.90.165.189}
+
+  
+server '164.90.165.189', user: 'deploy', roles: %w{app db web}
 
 
 # role-based syntax
